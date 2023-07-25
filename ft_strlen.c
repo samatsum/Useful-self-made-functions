782662bbc42ea5ad6020463e897cdb098148daa0
Samatsum <samatsum@student.42tokyo.jp>
2023-07-25 22:53:18 +0900

    OKOK

---
+++ ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samatsum  <samatsum@student.42tokyo.>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 20:48:11 by samatsum          #+#    #+#             */
+/*   Updated: 2023/05/16 21:56:02 by samatsum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+// #include<stdio.h>
+// int main(void)
+// {
+// 	int i = ft_strlen("01234");
+// 	printf("%d\n", i);
+// }
